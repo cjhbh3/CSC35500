61abccfdd6f000df6ed45028ed1bfcd9f1ae86ad
CJ Hess <cjh147@lindenwood.edu>
2022-09-28 20:59:58 -0500

    Project done

--- Project 1/main.cpp
+++ Project 1/main.cpp
@@ -12,8 +12,7 @@
 
 int main(void)
 {
-    Command com;
-    int num=1; // keep track of number fo commands. 
+    Command com; 
     
     // prompt for and read in first command. 
     cout << ">>>> ";
@@ -55,32 +54,148 @@ int main(void)
             }
 
             argList[argsFilled] = NULL;
-            
-            
-            if (fork() == 0) {
+
+
+            // 9/24: figured out how to redirect to another file
+            // Use open to open another file stream
+            if (com.redirOut()) {
+                int fd = open(com.outputRedirectFile().c_str(), O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+                dup2(fd, 1);
+                close(fd);
+            }
+
+            // 9/26: figured out how to redirect with input
+            if (com.redirIn()) {
+                char *c = (char *) calloc(100, sizeof(char));
+                int fd = open(com.inputRedirectFile().c_str(), O_RDONLY);
+                if (fd < 0) { perror("r1"); exit(1); }
+                int sz = read(fd, c, 10);
+            }
+
+
+            if (!com.pipeOut()) {
+                if (com.backgrounded()) {
+                    int pid = fork();
+                    if (pid != 0) {
+                        
+                    } 
+                    else {
+                        int pid = fork();
+                        if (pid != 0) {
+                            waitpid(pid, NULL, 0);
+                            cout<< "Completed: PID = " << pid << " :: Command = "<< com << endl;
+                            exit(1);
+                        }
+                        else {
+                            // Normal execute
+                            int status_code = execvp(argList[0], argList);
+
+                            if (status_code == -1) {
+                                cout << "Terminated Incorrectly" << endl;
+                                return 1;
+                            }
+                        }
+                    }
+                }
+                else {
+                    if (fork() == 0) {
+                        // Normal execute
+                        int status_code = execvp(argList[0], argList);
+
+                        if (status_code == -1) {
+                            cout << "Terminated Incorrectly" << endl;
+                            return 1;
+                        }
+                    }
+                    waitpid(0, NULL, 0);
+                }
+            }
+            else {
+                Command com2;  
+                int com2ArgListSize = com.numArgs() + 1;
+                int args2Filled = 0;
+                if (com2.redirIn()) { com2ArgListSize++; }
+                char * com2ArgList[com2ArgListSize];
+
+                com2.read();
+                const string * args2 = com2.args();
+
+                cout << com2 << endl;
+
+                /* Redo literally evertying we already did for com1*/
+                // Add the pipes
+
+               
+                
+                // First arg is command name, next are the flags, etc
+
+                for (int i = 0; i < com2.numArgs(); i++) {
+                    // Should iterate through the command's args and add them to our char argList
+                    // Just because that is what needed to pass through execvp() 
+                    com2ArgList[i] = (char*) com2.args()[i].c_str();
+                    args2Filled++;
+                }
+                
+
+                if (com2.redirIn()) {
+                    com2ArgList[argsFilled] = (char*)com2.inputRedirectFile().c_str();
+                    args2Filled++;
+                }
+
+                com2ArgList[argsFilled] = NULL;
+                
+
                 // 9/24: figured out how to redirect to another file
                 // Use open to open another file stream
-                if (com.redirOut()) {
-                    int fd = open(com.outputRedirectFile().c_str(), O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+                if (com2.redirOut()) {
+                    int fd = open(com2.outputRedirectFile().c_str(), O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
                     dup2(fd, 1);
                     close(fd);
                 }
 
-                // 9/26: figured out how to redirect with input, fucking feeling good.
-                if (com.redirIn()) {
+                // 9/26: figured out how to redirect with input
+                if (com2.redirIn()) {
                     char *c = (char *) calloc(100, sizeof(char));
-                    int fd = open(com.inputRedirectFile().c_str(), O_RDONLY);
+                    int fd = open(com2.inputRedirectFile().c_str(), O_RDONLY);
                     if (fd < 0) { perror("r1"); exit(1); }
                     int sz = read(fd, c, 10);
                 }
 
-                int status_code = execvp(argList[0], argList);
 
-                if (status_code == -1) {
-                    cout << "Terminated Incorrectly" << endl;
-                    return 1;
+                int p[2];
+                if (pipe(p) == -1) {
+                    perror("Pipe failed");
+                    exit(1);
                 }
+
+                if (fork() == 0) {
+                    close(STDOUT_FILENO);
+                    dup(p[1]);
+                    close(p[0]);
+                    close(p[1]);
+
+                    execvp(argList[0], argList);
+                    perror("execvp of 1st child");
+                    exit(1);
+                }
+
+                if (fork() == 0) {
+                    close(STDIN_FILENO);
+                    dup(p[0]);
+                    close(p[1]);
+                    close(p[0]);
+
+                    execvp(com2ArgList[0], com2ArgList);
+                    perror("execvp of 2nd child");
+                    exit(1);
+                }
+
+                close(p[0]);
+                close(p[1]);
+                wait(0);
+                wait(0);
             }
+                
         }
 
         // prompt for and read next command
